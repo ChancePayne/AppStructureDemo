15cc154a623caf903d15ca40c5a7e16c2bd889d2
Chance Payne <chance@lambdaschool.com>
2019-08-08 10:41:57 -0700

    update views with data

--- app/src/main/java/com/lambdaschool/appstructuredemo/ui/MainActivity.kt
+++ app/src/main/java/com/lambdaschool/appstructuredemo/ui/MainActivity.kt
@@ -5,6 +5,7 @@ import android.os.Bundle
 //import com.lambdaschool.appstructuredemo.util.
 import com.lambdaschool.appstructuredemo.R
 import com.lambdaschool.appstructuredemo.model.Order
+import com.lambdaschool.appstructuredemo.util.DateUtils
 import com.lambdaschool.appstructuredemo.util.TextUtils
 import kotlinx.android.synthetic.main.activity_main.*
 
@@ -15,14 +16,15 @@ class MainActivity : AppCompatActivity() {
         setContentView(R.layout.activity_main)
 
         title = "Order Details"
+
+        displayOrderDetails(Order())
     }
 
     private fun displayOrderDetails(order: Order) {
         discount_view.text = TextUtils.formatPercent(order.discount)
-        orderDate_view
-        price_view
-        tax_view
-        customer_first_view
-        customer_last_view
+        orderDate_view.text = DateUtils.formatDate(order.orderDate)
+        price_view.text = TextUtils.formatCurrency(order.price)
+        tax_view.text = TextUtils.formatCurrency(order.price)
+        customer_first_view.text = TextUtils.formatCustomerName(order.customerFirstName, order.customerLastName)
     }
 }
