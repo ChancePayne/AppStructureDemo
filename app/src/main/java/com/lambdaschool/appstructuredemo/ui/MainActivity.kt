d7616fea710647ecdb85314a570654b226011e5e
Chance Payne <chance@lambdaschool.com>
2019-08-08 10:17:14 -0700

    Initial commit

---
+++ app/src/main/java/com/lambdaschool/appstructuredemo/ui/MainActivity.kt
@@ -0,0 +1,13 @@
+package com.lambdaschool.appstructuredemo.ui
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.lambdaschool.appstructuredemo.R
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
